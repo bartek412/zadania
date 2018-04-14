9c566e47db60473111a5959b8a8b4cecda1b3946
Bartek412 <bartekg412@gmail.com>
2018-04-14 17:35:44 +0200

    Add project files.

---
+++ zadania/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zadania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
